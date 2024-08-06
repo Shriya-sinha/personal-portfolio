dd0f77c75239742e6521d209aa7e2b3a8dc0afcf
Shreya-sinha <sinhashreya2679@gmail.com>
2024-08-06 19:09:36 +0530

    feat: add Vercel configuration for client-side routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Link} from "react-router-dom";
 // import Contact from "./pages/contact";
 import Home from "./pages/home";
 import Projects from "./pages/projects";
 import About from "./pages/about";
 import Contact from "./pages/contact";
+
+
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/projects">Projects</Link>
+      </nav>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
