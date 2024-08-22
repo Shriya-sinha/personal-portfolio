8f00b2213c51413735870d0e1e70de03fd9f9d47
Shreya-sinha <sinhashreya2679@gmail.com>
2024-08-22 22:51:40 +0530

    fix : edit texts

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route,Link} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 // import Contact from "./pages/contact";
 import Home from "./pages/home";
 import Projects from "./pages/projects";
@@ -9,12 +9,6 @@ import Contact from "./pages/contact";
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/home">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/projects">Projects</Link>
-      </nav>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
--- Frontend/src/pages/about.jsx
+++ Frontend/src/pages/about.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 const About = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div
         className="grid grid-flow-row grid-cols-2 mx-16 py-16 auto"
         id="about"
@@ -20,26 +20,37 @@ const About = () => {
             About Shreya
           </h1>
           <hr className="w-10 my-8 flex justify-items-start border-cyen" />
-          <p className="uppercase text-base pb-5 tracking-wide">
+          {/* <p className="uppercase text-base pb-5 tracking-wide">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
             corporis voluptas inventore at.
+          </p> */}
+          <p className="text-base base whitespace-normal">
+            My coding odyssey began unexpectedly in 11th grade when my aversion
+            to Hindi's complex "vowel diacritics" (matras) led me to choose
+            Computer Science as my fifth subject. This decision opened the door
+            to a fascinating new world, with Python as my first programming
+            language. Alongside Python, I delved into MySQL, SQL queries, and
+            operating systems,Computer Networks, Data structures laying a solid foundation in computer science
+            fundamentals.
           </p>
-          <p className="text-xl base whitespace-normal">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-            pariatur, explicabo rem reprehenderit possimus earum nulla modi
-            atque eligendi velit placeat id et quaerat iusto non quos provident
+          <p className="text-base whitespace-normal pt-5">
+            Initially, pattern problems posed a challenge, but their intricacy
+            only fueled my curiosity. Programming became my escape—a sci-fi
+            realm where logic and creativity intertwined. Whenever the
+            complexities of the real world overwhelmed me, I found solace in
+            coding, each line of syntax offering a new adventure.
           </p>
-          <p className="text-xl base whitespace-normal pt-5">
-            ipsum voluptates? Quisquam totam sapiente necessitatibus, cum minima
-            ab iste blanditiis officiis nisi velit numquam sunt consectetur
-            fugiat voluptatum esse rem. Aperiam nostrum ad, incidunt nemo nulla
-            repellat unde veritatis ipsam ipsum. Odit sed voluptate,
+          <p className="text-base whitespace-normal pt-5">
+            This early passion ignited a fire that guided my career path towards
+            Software Development. While I'm deeply intrigued by machine learning
+            and its potential, my love for Python remains unwavering.
           </p>
-          <p className="text-xl base whitespace-normal pt-5">
-            ipsum voluptates? Quisquam totam sapiente necessitatibus, cum minima
-            ab iste blanditiis officiis nisi velit numquam sunt consectetur
-            fugiat voluptatum esse rem. Aperiam nostrum ad, incidunt nemo nulla
-            repellat unde veritatis ipsam ipsum. Odit sed voluptate,
+          <p className="text-base whitespace-normal pt-5">
+            Today, as I navigate the ever-expanding universe of technology, I
+            carry with me the same sense of wonder that first drew me to
+            programming. My journey from a Hindi-avoiding student to a dedicated
+            software developer is a testament to the transformative power of
+            finding one's true calling in the digital realm.
           </p>
         </div>
       </div>
